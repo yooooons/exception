a78ced4b50b2999c6a97e92301dde65cee7fa503
unknown <yooooonnf@gmail.com>
2022-03-20 23:11:06 +0900

    API 예외 처리

--- src/main/java/hello/exception/servlet/ErrorPageController.java
+++ src/main/java/hello/exception/servlet/ErrorPageController.java
@@ -1,11 +1,17 @@
 package hello.exception.servlet;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -34,14 +40,27 @@ public class ErrorPageController {
         return "error-page/500";
     }
 
+    @RequestMapping(value = "/error-page/500", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Map<String, Object>> errorPage500Api(HttpServletRequest request, HttpServletResponse response) {
+        log.info("API errorPage 500");
+        HashMap<String, Object> result = new HashMap<>();//HashMap은 순서 보장 x
+        Exception ex = (Exception) request.getAttribute(ERROR_EXCEPTION);
+        result.put("status", request.getAttribute(ERROR_STATUS_CODE));
+        result.put("message", ex.getMessage());
+
+        Integer statusCode =(Integer) request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        return new ResponseEntity<>(result, HttpStatus.valueOf(statusCode));
+    }
+
     private void printErrorInfo(HttpServletRequest request) {
-        log.info("ERROR_EXCEPTION:   {}",request.getAttribute(ERROR_EXCEPTION));
-        log.info("ERROR_EXCEPTION_TYPE:   {}",request.getAttribute(ERROR_EXCEPTION_TYPE));
-        log.info("ERROR_MESSAGE:   {}",request.getAttribute(ERROR_MESSAGE));
-        log.info("ERROR_REQUEST_URI:   {}",request.getAttribute(ERROR_REQUEST_URI));
-        log.info("ERROR_SERVLET_NAME:   {}",request.getAttribute(ERROR_SERVLET_NAME));
-        log.info("ERROR_STATUS_CODE:   {}",request.getAttribute(ERROR_STATUS_CODE));
-        log.info("dispatchType={}",request.getDispatcherType());
+
+        log.info("ERROR_EXCEPTION:   {}", request.getAttribute(ERROR_EXCEPTION));
+        log.info("ERROR_EXCEPTION_TYPE:   {}", request.getAttribute(ERROR_EXCEPTION_TYPE));
+        log.info("ERROR_MESSAGE:   {}", request.getAttribute(ERROR_MESSAGE));
+        log.info("ERROR_REQUEST_URI:   {}", request.getAttribute(ERROR_REQUEST_URI));
+        log.info("ERROR_SERVLET_NAME:   {}", request.getAttribute(ERROR_SERVLET_NAME));
+        log.info("ERROR_STATUS_CODE:   {}", request.getAttribute(ERROR_STATUS_CODE));
+        log.info("dispatchType={}", request.getDispatcherType());
     }
 
 }
